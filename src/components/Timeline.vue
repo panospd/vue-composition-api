1cdddc4154511225552109c26eaebda48d22ec6a
Panos Anastasiadis <anastapd@gmail.com>
2021-09-05 10:39:49 +0100

    Implemented Timeline

---
+++ src/components/Timeline.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a 
+        v-for="period in periods" 
+        :key="period"
+        :class="{'is-active': currentPeriod === period}"
+        :data-test="period"
+        @click="setPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+    <a 
+      v-for="post in posts" 
+      :key="post.id"
+      class="panel-block"
+    >
+      <a>{{ post.title }}</a>
+      <div>{{ post.created.format('Do MMM') }}</div>
+    </a>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+import moment from "moment"
+import { today, thisWeek, thisMonth } from "../mocks"
+
+type Period = "Today" | "This Week" | "This Month"
+
+export default defineComponent({
+  name: 'Timeline',
+  setup() {
+    const periods: Period[] = ["Today", "This Week", "This Month"]
+    const currentPeriod = ref<Period>(periods[0])
+    const posts = computed(() => {
+      return [today, thisWeek, thisMonth].filter(post => {
+        if (currentPeriod.value === 'Today') {
+          return post.created.isAfter(moment().subtract(1, 'day'))
+        }
+
+        if (currentPeriod.value === "This Week") {
+          return post.created.isAfter(moment().subtract(1, 'week'))
+        }
+
+        if (currentPeriod.value === 'This Month') {
+          return post.created.isAfter(moment().subtract(1, 'month'))
+        }
+
+        return false
+      })
+    }) 
+
+    const setPeriod = (period: Period) => {
+      currentPeriod.value = period
+    }
+
+    return {
+      periods,
+      currentPeriod,
+      setPeriod,
+      posts
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
